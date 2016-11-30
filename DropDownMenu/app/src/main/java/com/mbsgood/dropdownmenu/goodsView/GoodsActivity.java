d78127dff3164bfc91f0784ce03d63e1a3aa332f
MbsGood <chenchaoxue@vip.qq.com>
2016-11-30 11:45:30 +0800

    git android studio提交测试

---
+++ DropDownMenu/app/src/main/java/com/mbsgood/dropdownmenu/goodsView/GoodsActivity.java
@@ -0,0 +1,20 @@
+package com.mbsgood.dropdownmenu.goodsView;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/*
+* User: ChenCHaoXue
+* Create date: 2016-11-30 
+* Time: 11:24 
+* From VCard
+* 外卖下订单
+*/
+public class GoodsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
